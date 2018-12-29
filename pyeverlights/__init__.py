fb8456716cb9c1924e3e99d6a3f47c8f3a36e2a6
Jon Caruana <jon@joncaruana.com>
2018-12-29 08:29:27 +0000

    Lint cleanup.

--- pyeverlights/__init__.py
+++ pyeverlights/__init__.py
@@ -18,62 +18,72 @@ MODE_RB = 'rb'
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class ConnectionError(Exception):
-  pass
+    pass
+
 
 class EverLights:
-  def __init__(self, ip, session=None):
-    self._ip = ip
-    self._session = session
-    self._auto_session = False
+    def __init__(self, ip, session=None):
+        self._ip = ip
+        self._session = session
+        self._auto_session = False
 
-  async def _fetch(self, path, params=None):
-    if not self._session:
-      self._session = aiohttp.ClientSession()
-      self._auto_session = True
+    async def _fetch(self, path, params=None):
+        if not self._session:
+            self._session = aiohttp.ClientSession()
+            self._auto_session = True
 
-    try:
-      async with self._session.get('http://'+self._ip+path, params=params) as response:
-        data = await response.json()
-        _LOGGER.debug(str(response.url)+' response: '+json.dumps(data, sort_keys=True, indent=4))
-        return data
-    except aiohttp.client_exceptions.ClientConnectorError as e:
-      raise ConnectionError from e
-    except asyncio.TimeoutError as e:
-      raise ConnectionError from e
-    except json.decoder.JSONDecodeError as e:
-      raise ConnectionError from e
+        try:
+            async with self._session.get('http://'+self._ip+path,
+                                         params=params) as response:
+                data = await response.json()
+                _LOGGER.debug(str(response.url) + ' response: ' +
+                              json.dumps(data, sort_keys=True, indent=4))
+                return data
+        except aiohttp.client_exceptions.ClientConnectorError as e:
+            raise ConnectionError from e
+        except asyncio.TimeoutError as e:
+            raise ConnectionError from e
+        except json.decoder.JSONDecodeError as e:
+            raise ConnectionError from e
 
-  async def get_status(self):
-    resp = await self._fetch('/status/get')
-    return resp
+    async def get_status(self):
+        resp = await self._fetch('/status/get')
+        return resp
 
-  async def set_pattern(self, channel, colors=[], modes={}):
-    data = {
-      'len': len(colors),
-      'ptrn': colors,
-      'modes': modes,
-      'name': ''
-    }
-    await self._fetch('/ptrn/set', {'channel': channel, 'data': json.dumps(data)})
+    async def set_pattern(self, channel, colors=[], modes={}):
+        data = {
+            'len': len(colors),
+            'ptrn': colors,
+            'modes': modes,
+            'name': ''
+        }
+        await self._fetch('/ptrn/set', {
+            'channel': channel,
+            'data': json.dumps(data)
+        })
 
-  async def set_pattern_by_id(self, channel, id):
-    data = await self.get_pattern(id)
-    await self._fetch('/ptrn/set', {'channel': channel, 'data': json.dumps(data)})
-    return data['ptrn']
+    async def set_pattern_by_id(self, channel, id):
+        data = await self.get_pattern(id)
+        await self._fetch('/ptrn/set', {
+            'channel': channel,
+            'data': json.dumps(data)
+        })
+        return data['ptrn']
 
-  async def clear_pattern(self, channel):
-    await self._fetch('/ptrn/clear', {'channel': channel})
+    async def clear_pattern(self, channel):
+        await self._fetch('/ptrn/clear', {'channel': channel})
 
-  async def get_all_patterns(self):
-    resp = await self._fetch('/ptrn/all')
-    return resp['patterns']
+    async def get_all_patterns(self):
+        resp = await self._fetch('/ptrn/all')
+        return resp['patterns']
 
-  async def get_pattern(self, id):
-    return await self._fetch('/ptrn/get', {'id': id})
+    async def get_pattern(self, id):
+        return await self._fetch('/ptrn/get', {'id': id})
 
-  async def close(self):
-    if self._auto_session:
-      await self._session.close()
-      self._session = None
-      self._auto_session = False
+    async def close(self):
+        if self._auto_session:
+            await self._session.close()
+            self._session = None
+            self._auto_session = False
